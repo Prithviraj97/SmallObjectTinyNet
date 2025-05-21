524fdfacd120d917f801166656098f2526e06152
Prithvi Raj Singh <prithvirajsingh358@gmail.com>
2025-05-21 11:35:03 -0500

    improved training code

--- training/train.py
+++ training/train.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
 from torch.utils.data import DataLoader
 import sys
 from pathlib import Path
@@ -10,18 +11,23 @@ from data.synthetic_dataset import SyntheticDotDataset
 from models.tinyCNN import TinyCNNRegressor  
 
 # Hyperparameters
-epochs = 10
-batch_size = 64
+epochs = 20
+batch_size = 16
 lr = 1e-3
 
 # Data
 train_dataset = SyntheticDotDataset(num_sequences=500, frames_per_seq=10)
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
-# Model + Loss + Optimizer
+#Evaluation
+test_dataset = SyntheticDotDataset(num_sequences=50, frames_per_seq=10)
+test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+
+# Model
 model = TinyCNNRegressor()
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=lr)
+train_loss_histroy = []
 
 # Training Loop
 for epoch in range(epochs):
@@ -36,4 +42,35 @@ for epoch in range(epochs):
         running_loss += loss.item() * imgs.size(0)
 
     epoch_loss = running_loss / len(train_loader.dataset)
+    train_loss_histroy.append(epoch_loss)
     print(f"Epoch {epoch+1}/{epochs} - Loss: {epoch_loss:.4f}")
+
+# Save the model
+torch.save(model.state_dict(), "tinyCNN_model.pth")
+
+'''
+Evlaution and Visualization
+'''
+model.eval()
+predicted_positions = []
+true_positions = []
+with torch.no_grad():
+    for imgs, labels in test_loader:
+        outputs = model(imgs)
+        predicted_positions.append(outputs.squeeze().numpy())
+        true_positions.append(labels.squeeze().numpy())
+
+predicted_positions = torch.tensor(predicted_positions)
+true_positions = torch.tensor(true_positions)
+
+# Visualize the predicted and true positions
+plt.figure(figsize=(6, 6))
+plt.scatter(true_positions[:, 0], true_positions[:, 1], label='True', c='blue', alpha=0.6)
+plt.scatter(predicted_positions[:, 0], predicted_positions[:, 1], label='Predicted', c='red', marker='x')
+plt.title("Predicted vs Actual Object Positions")
+plt.xlabel("x position")
+plt.ylabel("y position")
+plt.legend()
+plt.grid(True)
+plt.axis("equal")
+plt.show()
